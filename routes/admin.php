805d416c101ff917b1e353becf17c1a3a45e0cd1
Mohammed Abu Yousef <m.abuyousef@newsolutions.ps>
2023-07-16 19:29:20 +0300

    ...

--- routes/admin.php
+++ routes/admin.php
@@ -8,8 +8,9 @@ use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth', 'verified'])->group(function () {
-
-    Route::get('/', [DashboardController::class, 'index']);
+    Route::get('/', function () {
+        return redirect()->route('dashboard');
+    });
 
     Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
 
