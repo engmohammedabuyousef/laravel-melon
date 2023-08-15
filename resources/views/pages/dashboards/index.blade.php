125a8ca87a72cce4e76d6f3d73fc9e833765ca3e
Mohammed Abu Yousef <m.abuyousef@newsolutions.ps>
2023-08-15 12:49:26 +0300

    ...

--- resources/views/pages/dashboards/index.blade.php
+++ resources/views/pages/dashboards/index.blade.php
@@ -1,3 +1,30 @@
 <x-default-layout>
-    Hello
+    <div class="row g-5 g-xl-8">
+        <div class="col-xl-3">
+            <a href="#" class="card bg-warning hoverable card-xl-stretch mb-xl-8">
+                <!--begin::Body-->
+                <div class="card-body">
+                    <i class="ki-duotone ki-briefcase text-white fs-2x ms-n1">
+                        <span class="path1"></span><span class="path2"></span>
+                    </i>
+                    <div class="text-white fw-bold fs-2 mb-2 mt-5">195</div>
+                    <div class="fw-semibold text-white">Users</div>
+                </div>
+                <!--end::Body-->
+            </a>
+        </div>
+
+        <div class="col-xl-3">
+            <a href="#" class="card bg-info hoverable card-xl-stretch mb-5 mb-xl-8">
+                <!--begin::Body-->
+                <div class="card-body">
+                    <i class="ki-duotone ki-chart-pie-simple text-white fs-2x ms-n1">
+                        <span class="path1"></span><span class="path2"></span></i>
+                    <div class="text-white fw-bold fs-2 mb-2 mt-5">5</div>
+                    <div class="fw-semibold text-white">Admins</div>
+                </div>
+                <!--end::Body-->
+            </a>
+        </div>
+    </div>
 </x-default-layout>
