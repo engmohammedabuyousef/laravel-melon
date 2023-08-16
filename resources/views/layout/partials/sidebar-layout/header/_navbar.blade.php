d5c6875ccf9200474a7dc49d4fe674d2022b0727
Mohammed Abu Yousef <m.abuyousef@newsolutions.ps>
2023-08-16 18:04:49 +0300

    ...

--- resources/views/layout/partials/sidebar-layout/header/_navbar.blade.php
+++ resources/views/layout/partials/sidebar-layout/header/_navbar.blade.php
@@ -2,7 +2,7 @@
 <div class="app-navbar flex-shrink-0">
 
     <!--begin::Notifications-->
-	{{-- <div class="app-navbar-item ms-1 ms-md-4">
+    {{-- <div class="app-navbar-item ms-1 ms-md-4">
         <!--begin::Menu- wrapper-->
 		<div class="btn btn-icon btn-custom btn-icon-muted btn-active-light btn-active-color-primary w-35px h-35px" data-kt-menu-trigger="{default: 'click', lg: 'hover'}" data-kt-menu-attach="parent" data-kt-menu-placement="bottom-end" id="kt_menu_item_wow">{!! getIcon('notification-status', 'fs-2') !!}</div>
         @include('partials/menus/_notifications-menu')
@@ -11,7 +11,7 @@
     <!--end::Notifications-->
 
     <!--begin::Test menu-->
-	{{-- <div class="app-navbar-item ms-1 ms-md-4" id="kt_header_user_menu_toggle">
+    {{-- <div class="app-navbar-item ms-1 ms-md-4" id="kt_header_user_menu_toggle">
         <!--begin::Menu wrapper-->
 		<div class="cursor-pointer symbol symbol-70px" data-kt-menu-trigger="{default: 'click', lg: 'hover'}" data-kt-menu-attach="parent" data-kt-menu-placement="bottom-end">
             <div  class="symbol-label fs-3 bg-light-danger text-danger">
@@ -23,10 +23,10 @@
     <!--end::Test menu-->
 
     <!--begin::Lang menu-->
-	{{-- <div class="app-navbar-item ms-1 ms-md-4" id="kt_header_user_menu_toggle">
+    {{-- <div class="app-navbar-item ms-1 ms-md-4" id="kt_header_user_menu_toggle">
         <!--begin::Menu wrapper-->
 		<div class="cursor-pointer symbol symbol-70px" data-kt-menu-trigger="{default: 'click', lg: 'hover'}" data-kt-menu-attach="parent" data-kt-menu-placement="bottom-end">
-            @if(app()->getLocale() == 'ar')
+            @if (app()->getLocale() == 'ar')
             <a href="{{ route('en') }}" class="symbol-label fs-3 bg-light-primary text-primary">
                 {{ 'English' }}
             </a>
@@ -41,13 +41,15 @@
     <!--end::Lang menu-->
 
     <!--begin::User menu-->
-	<div class="app-navbar-item ms-1 ms-md-4" id="kt_header_user_menu_toggle">
+    <div class="app-navbar-item ms-1 ms-md-4" id="kt_header_user_menu_toggle">
         <!--begin::Menu wrapper-->
-		<div class="cursor-pointer symbol symbol-35px" data-kt-menu-trigger="{default: 'click', lg: 'hover'}" data-kt-menu-attach="parent" data-kt-menu-placement="bottom-end">
-            @if(Auth::guard('admin')->user()->photo)
-                <img src="{{ \Auth::guard('admin')->user()->photo }}" class="rounded-3" alt="user" />
+        <div class="cursor-pointer symbol symbol-35px" data-kt-menu-trigger="{default: 'click', lg: 'hover'}"
+            data-kt-menu-attach="parent" data-kt-menu-placement="bottom-end">
+            @if (Auth::guard('admin')->user()->photo)
+                <img src="{{ Auth::guard('admin')->user()->photo }}" class="rounded-3" alt="user" />
             @else
-                <div class="symbol-label fs-3 {{ app(\App\Actions\GetThemeType::class)->handle('bg-light-? text-?', Auth::guard('admin')->user()->name) }}">
+                <div
+                    class="symbol-label fs-3 {{ app(\App\Actions\GetThemeType::class)->handle('bg-light-? text-?', Auth::guard('admin')->user()->name) }}">
                     {{ substr(Auth::guard('admin')->user()->name, 0, 1) }}
                 </div>
             @endif
@@ -57,11 +59,12 @@
     </div>
     <!--end::User menu-->
     <!--begin::Header menu toggle-->
-	<div class="app-navbar-item d-lg-none ms-2 me-n2" title="Show header menu">
-		<div class="btn btn-flex btn-icon btn-active-color-primary w-30px h-30px" id="kt_app_header_menu_toggle">{!! getIcon('element-4', 'fs-1') !!}</div>
+    <div class="app-navbar-item d-lg-none ms-2 me-n2" title="Show header menu">
+        <div class="btn btn-flex btn-icon btn-active-color-primary w-30px h-30px" id="kt_app_header_menu_toggle">
+            {!! getIcon('element-4', 'fs-1') !!}</div>
     </div>
     <!--end::Header menu toggle-->
-	<!--begin::Aside toggle-->
-	<!--end::Header menu toggle-->
+    <!--begin::Aside toggle-->
+    <!--end::Header menu toggle-->
 </div>
 <!--end::Navbar-->
