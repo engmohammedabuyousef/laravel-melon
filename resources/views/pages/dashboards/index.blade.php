8090dcd1eddfe09422eb018dc738692592b62d67
Mohammed Abu Yousef <m.abuyousef@newsolutions.ps>
2023-10-06 14:33:02 +0300

    .

--- resources/views/pages/dashboards/index.blade.php
+++ resources/views/pages/dashboards/index.blade.php
@@ -1,32 +1,29 @@
 <x-default-layout>
-    <div class="row g-5 g-xl-8">
+    @if(authAdmin()->hasPermissionTo('dashboard'))
+        <div class="row g-5 g-xl-8">
+            <div class="col-xl-3">
+                <a href="#" class="card bg-info hoverable card-m-stretch mb-5 mb-xl-8">
+                    <!--begin::Body-->
+                    <div class="card-body">
+                        <div class="text-white fw-bold fs-2 mb-2 mt-2">{{ $admins_count }}</div>
+                        <div class="fw-semibold text-white">Admins</div>
+                    </div>
+                    <!--end::Body-->
+                </a>
+            </div>
 
-        <div class="col-xl-3">
-            <a href="#" class="card bg-info hoverable card-xl-stretch mb-5 mb-xl-8">
-                <!--begin::Body-->
-                <div class="card-body">
-                    <i class="ki-duotone ki-chart-pie-simple text-white fs-2x ms-n1">
-                        <span class="path1"></span><span class="path2"></span></i>
-                    <div class="text-white fw-bold fs-2 mb-2 mt-5">{{ $admins_count }}</div>
-                    <div class="fw-semibold text-white">Admins</div>
-                </div>
-                <!--end::Body-->
-            </a>
+            <div class="col-xl-3">
+                <a href="#" class="card bg-warning hoverable card-m-stretch mb-xl-8">
+                    <!--begin::Body-->
+                    <div class="card-body">
+                        <div class="text-white fw-bold fs-2 mb-2 mt-2">{{ $users_count }}</div>
+                        <div class="fw-semibold text-white">Users</div>
+                    </div>
+                    <!--end::Body-->
+                </a>
+            </div>
         </div>
-
-        <div class="col-xl-3">
-            <a href="#" class="card bg-warning hoverable card-xl-stretch mb-xl-8">
-                <!--begin::Body-->
-                <div class="card-body">
-                    <i class="ki-duotone ki-briefcase text-white fs-2x ms-n1">
-                        <span class="path1"></span><span class="path2"></span>
-                    </i>
-                    <div class="text-white fw-bold fs-2 mb-2 mt-5">{{ $users_count }}</div>
-                    <div class="fw-semibold text-white">Users</div>
-                </div>
-                <!--end::Body-->
-            </a>
-        </div>
-
-    </div>
+    @else
+        <div>Welcome</div>
+    @endif
 </x-default-layout>
