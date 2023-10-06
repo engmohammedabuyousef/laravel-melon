a0914d6d4f5ea0ae2c4ce0ca5bc59a5bb237fb78
Mohammed Abu Yousef <m.abuyousef@newsolutions.ps>
2023-10-06 06:17:05 +0300

    .

--- resources/views/partials/_sections.blade.php
+++ resources/views/partials/_sections.blade.php
@@ -18,63 +18,83 @@
             $is_true = 1;
         }
         ?>
-
-    <li class="menu-item menu-item-submenu @if ($is_true) menu-item-active menu-item-open @endif"
-        aria-haspopup="true" data-menu-toggle="hover">
-        <a href="{{ $menu['page'] }}" class="menu-link menu-toggle">
-{{--            @if (isset($isSub) && $isSub)--}}
-{{--                <i class="{{ $menu['icon'] }}">--}}
-{{--                    <span></span>--}}
-{{--                </i>--}}
-{{--            @else--}}
-{{--                <span class="svg-icon menu-icon">--}}
-{{--                        <span class="svg-icon svg-icon-primary svg-icon-2x">--}}
-{{--                            {!! $menu['icon'] !!}--}}
-{{--                        </span>--}}
-{{--                    </span>--}}
-{{--            @endif--}}
-
-            <span class="menu-text">{{ $menu['title'] }}</span>
+    {{--
+        <li class="menu-item menu-item-submenu @if ($is_true) menu-item-active menu-item-open @endif"
+            aria-haspopup="true" data-menu-toggle="hover">
+            <a href="{{ $menu['page'] }}" class="menu-link menu-toggle">
+                <span class="menu-text">{{ $menu['title'] }}</span>
+                @if (isset($menu['submenu']) && count($menu['submenu']) > 0)
+                    <i class="menu-arrow"></i>
+                @endif
+            </a>
             @if (isset($menu['submenu']) && count($menu['submenu']) > 0)
-                <i class="menu-arrow"></i>
+                <div class="menu-submenu">
+                    <i class="menu-arrow"></i>
+                    <ul class="menu-subnav">
+                        @include('partials._sections', ['menus' => $menu['submenu'], 'isSub' => true])
+                    </ul>
+                </div>
             @endif
-        </a>
-        @if (isset($menu['submenu']) && count($menu['submenu']) > 0)
-            <div class="menu-submenu">
-                <i class="menu-arrow"></i>
-                <ul class="menu-subnav">
-                    @include('partials._sections', ['menus' => $menu['submenu'], 'isSub' => true])
-                </ul>
-            </div>
-        @endif
-    </li>
+        </li>
+    --}}
 
+{{--    {{$menu['permission']}}--}}
+        {{$page}}
+{{--    {{$menu['link']}}--}}
     <div data-kt-menu-trigger="click"
-         class="menu-item menu-accordion {{ Illuminate\Support\Str::startsWith(request()->route()->getName(),['notifications'])? 'here show': '' }}">
+         class="menu-item menu-accordion
+{{--         {{ Illuminate\Support\Str::startsWith(request()->route()->getName(),['notifications'])? 'here show': '' }}--}}
+{{ $is_true ? 'here show' : '' }}
+         ">
         <!--begin:Menu link-->
         <span class="menu-link">
                     <span class="menu-icon">{!! getIcon('element-11', 'fs-2') !!}</span>
                     <span class="menu-title">{{ $menu['permission'] }}</span>
-                    <span class="menu-arrow"></span>
+
+
+                            @if (isset($menu['submenu']) && count($menu['submenu']) > 0)
+                <span class="menu-arrow"></span>
+            @endif
+
+
                 </span>
         <!--end:Menu link-->
+
+
+        @if (isset($menu['submenu']) && count($menu['submenu']) > 0)
+
         <!--begin:Menu sub-->
         <div class="menu-sub menu-sub-accordion">
             <!--begin:Menu item-->
             <div class="menu-item">
                 <!--begin:Menu link-->
-                <a class="menu-link {{ Illuminate\Support\Str::startsWith(request()->route()->getName(), ['notifications']) ? 'active': '' }}"
-                   href="{{ route('notifications.create') }}">
-                        <span class="menu-bullet">
-                                <span class="bullet bullet-dot"></span>
-                            </span>
-                    <span class="menu-title">{{ $menu['permission'] }}</span>
-                </a>
+                @include('partials._sections', ['menus' => $menu['submenu'], 'isSub' => true])
+
+                {{--                <a class="menu-link {{ Illuminate\Support\Str::startsWith(request()->route()->getName(), ['notifications']) ? 'active': '' }}"--}}
+{{--                   href="{{ route('notifications.create') }}">--}}
+{{--                        <span class="menu-bullet">--}}
+{{--                                <span class="bullet bullet-dot"></span>--}}
+{{--                            </span>--}}
+{{--                    <span class="menu-title">{{ $menu['permission'] }}</span>--}}
+{{--                </a>--}}
                 <!--end:Menu link-->
             </div>
             <!--end:Menu item-->
         </div>
         <!--end:Menu sub-->
+        @endif
+
+
     </div>
 
 @endforeach
+
+
+{{--@if (isset($menu['submenu']) && count($menu['submenu']) > 0)--}}
+{{--    <div class="menu-submenu">--}}
+{{--        <i class="menu-arrow"></i>--}}
+{{--        <ul class="menu-subnav">--}}
+{{--            @include('partials._sections', ['menus' => $menu['submenu'], 'isSub' => true])--}}
+{{--        </ul>--}}
+{{--    </div>--}}
+{{--@endif--}}
