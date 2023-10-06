5df55dcfdd48d502d3e7173600901768e145b372
Mohammed Abu Yousef <m.abuyousef@newsolutions.ps>
2023-10-06 06:47:10 +0300

    .

--- resources/views/partials/_sections.blade.php
+++ resources/views/partials/_sections.blade.php
@@ -22,13 +22,20 @@
          class="menu-item menu-accordion
          {{ Illuminate\Support\Str::startsWith(request()->route()->getName(),[ $menu['permission'] ])? 'here show': '' }}">
         <!--begin:Menu link-->
-        <span class="menu-link">
-            <span class="menu-icon">{!! getIcon('element-11', 'fs-2') !!}</span>
-            <span class="menu-title">{{ $menu['permission'] }}</span>
-            @if (isset($menu['submenu']) && count($menu['submenu']) > 0)
-                <span class="menu-arrow"></span>
-            @endif
-        </span>
+        @if($menu['link'] != '#')
+        <a href="{{route($menu['link'])}}">
+        @endif
+            <span class="menu-link">
+                <span class="menu-icon">{!! getIcon('element-11', 'fs-2') !!}</span>
+                <span class="menu-title">{{ $menu['permission'] }}</span>
+                @if (isset($menu['submenu']) && count($menu['submenu']) > 0)
+                    <span class="menu-arrow"></span>
+                @endif
+            </span>
+
+            @if($menu['link'] != '#')
+        </a>
+                    @endif
         <!--end:Menu link-->
 
         @if (isset($menu['submenu']) && count($menu['submenu']) > 0)
